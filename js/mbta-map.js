9a6fbb87648a375b1d81486c097086db872feead
DoubleOhh <114524539+DoubleOhh@users.noreply.github.com>
2024-12-04 23:57:56 -0500

    Testing the lines for the map
    The map works now i have to manually update the json file to get a kind of accurate map

--- js/mbta-map.js
+++ js/mbta-map.js
@@ -1,96 +1,34 @@
 var margin = {top: 50, right: 50, bottom: 70, left: 85},
-    width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+width = 500 - margin.left - margin.right,
+height = 500 - margin.top - margin.bottom;
+selectableElements = d3.select(null);
 
-// Define scales
-const xScale = d3.scaleLinear();
-const yScale = d3.scaleLinear();
-
-// Define the SVG and dimensions
+// Select the container
 var svg = d3.select("#mbta-map")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
+// Define the tube map
 var map = d3
-    .tubeMap()
-    .width(width * 0.9)
-    .height(height * 0.9)
+  .tubeMap()
+  .width(width)
+  .height(height)
+  .margin({
+    top: height / 50,
+    right: width / 7,
+    bottom: height / 10,
+    left: width / 7,
+  });
 
-// Load the MBTA data
-d3.json('../data/transit_data.json', function (error, data) {
+// Load the data using d3.json with a callback
+d3.json("./data/testing line.json", function (error, data) {
   if (error) {
     console.error("Error loading data:", error);
     return;
   }
-
   console.log(data);
 
-  const stations = data.stations;
-  const lines = data.lines;
-
-  // Compute the scale domains
-  const minX = d3.min(Object.values(stations), d => d.x);
-  const maxX = d3.max(Object.values(stations), d => d.x);
-  const minY = d3.min(Object.values(stations), d => d.y);
-  const maxY = d3.max(Object.values(stations), d => d.y);
-
-  const xScale = d3.scaleLinear().domain([minX, maxX]).range([margin.left, width - margin.right]);
-  const yScale = d3.scaleLinear().domain([minY, maxY]).range([height - margin.bottom, margin.top]);
-
-  // Draw the lines
-  lines.forEach(line => {
-    const linePath = d3.line()
-      .x(d => xScale(stations[d.name].x))
-      .y(d => yScale(stations[d.name].y));
-
-    svg.append("path")
-      .datum(line.nodes)
-      .attr("class", "line")
-      .attr("d", linePath)
-      .attr("stroke", line.color)
-      .attr("stroke-width", 2)
-      .attr("fill", "none");
-  });
-
-  // Draw the stations
-  svg.selectAll(".station")
-    .data(Object.values(stations))
-    .enter()
-    .append("circle")
-    .attr("class", "station")
-    .attr("r", 5)
-    .attr("cx", d => xScale(d.x))
-    .attr("cy", d => yScale(d.y))
-    .attr("fill", "black") // Explicitly set fill color
-    .on("click", (event, d) => showStationDetails(d))
-    .on("mouseover", function () {
-      d3.select(this).attr("fill", "red");
-    })
-    .on("mouseout", function () {
-      d3.select(this).attr("fill", "black");
-    });
-
-  // Add station labels
-  svg.selectAll(".label")
-    .data(Object.values(stations))
-    .enter()
-    .append("text")
-    .attr("class", "label")
-    .attr("x", d => xScale(d.x))
-    .attr("y", d => yScale(d.y) - 10)
-    .attr("text-anchor", "middle") // Center text
-    .attr("font-size", "10px")
-    .text(d => d.label);
+  // Bind the data to the container and call the tube map
+  svg.datum(data).call(map);
 });
-
-// Function to display station details in the sidebar
-function showStationDetails(station) {
-  const sidebar = d3.select("#sidebar");
-  sidebar.html(`
-    <h3>${station.label}</h3>
-    <p>Coordinates: (${station.x.toFixed(5)}, ${station.y.toFixed(5)})</p>
-  `);
-}
