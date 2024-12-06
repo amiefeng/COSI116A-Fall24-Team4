f375bef48b408dd08c648dd53d01c7b727531a93
DoubleOhh <114524539+DoubleOhh@users.noreply.github.com>
2024-12-06 14:53:24 -0500

    testing map

--- js/mbta-map.js
+++ js/mbta-map.js
@@ -1,13 +1,15 @@
 var margin = {top: 50, right: 50, bottom: 70, left: 85},
-width = 500 - margin.left - margin.right,
-height = 500 - margin.top - margin.bottom;
+width = 1600- margin.left - margin.right,
+height = 1024 - margin.top - margin.bottom;
 selectableElements = d3.select(null);
 
 // Select the container
 var svg = d3.select("#mbta-map")
   .append("svg")
-  .attr("width", width)
-  .attr("height", height);
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Define the tube map
 var map = d3
@@ -21,14 +23,16 @@ var map = d3
     left: width,
   });
 
-// Load the data using d3.json with a callback
+// Load the data
 d3.json("./data/testing line.json", function (error, data) {
   if (error) {
     console.error("Error loading data:", error);
     return;
   }
+  
   console.log(data);
 
   // Bind the data to the container and call the tube map
   svg.datum(data).call(map);
+  
 });
