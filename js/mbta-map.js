3e715444ce16f3cfccede9fef6fa49ccc4588f49
DoubleOhh <114524539+DoubleOhh@users.noreply.github.com>
2024-12-11 23:34:40 -0500

    the breakfast for dinner sucked also i did stuff

--- js/mbta-map.js
+++ js/mbta-map.js
@@ -1,37 +1,3 @@
-// var margin = {top: 50, right: 50, bottom: 70, left: 85},
-// width = 500 - margin.left - margin.right,
-// height = 500 - margin.top - margin.bottom;
-// selectableElements = d3.select(null);
-
-// // Select the container
-// var svg = d3.select("#mbta-map")
-//   .append("svg")
-//   .attr("width", width)
-//   .attr("height", height);
-
-// // Define the tube map
-// var map = d3
-//   .tubeMap()
-//   .width(width)
-//   .height(height)
-//   .margin({
-//     top: height ,
-//     right: width ,
-//     bottom: height ,
-//     left: width,
-//   });
-
-// // Load the data using d3.json with a callback
-// d3.json("./data/london-tube.json", function (error, data) {
-//   if (error) {
-//     console.error("Error loading data:", error);
-//     return;
-//   }
-//   console.log(data);
-
-//   // Bind the data to the container and call the tube map
-//   svg.datum(data).call(map);
-// });
 var container = d3.select("#mbta-map");
 var width = 1600;
 var height = 1600;
@@ -50,7 +16,7 @@ var map = d3
     console.log(name);
   });
 
-d3.json("./data/london-tube.json", function (error, data) {
+d3.json("./data/testingline.json", function (error, data) {
   container.datum(data).call(map);
 
   var svg = container.select("svg");
