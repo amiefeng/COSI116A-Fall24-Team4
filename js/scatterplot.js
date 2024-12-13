7b42b00eeeb4c382488c3d4a5a1cda60d0d883d5
PADELIYI <adeliyi.promise@gmail.com>
2024-12-13 14:58:32 -0500

    Fixing navbar sticky bug and scatterplot axis labels

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -65,8 +65,8 @@ function scatterplot(){
         // add y-axis label
         svg.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x",0 - (height / 2))
+        .attr("y", 10 - margin.left)
+        .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .text("Average Monthly Ridership"); 
