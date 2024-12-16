d28b7f5976da413a1f8d25942fd0a74265f16c01
DoubleOhh <114524539+DoubleOhh@users.noreply.github.com>
2024-12-15 19:24:47 -0500

    fixed filter

--- js/visualization.js
+++ js/visualization.js
@@ -1,9 +1,9 @@
-const dispatchString = "selectionUpdated"; //this updates the selection of the points on the scatterplot
+const dispatchString = "selectionUpdated"; //this updates the selection of the points on the scatterplot and lines on map
 const dispatchMonth = "monthUpdated"; //this corresponds ot a specific combination of month/year, with the points on the scatterplot
-                                      //corresponding to this combination being selected
+
 const dispatchLine = "lineUpdated"
 
-                                      //create dispatchers for different events
+//create dispatchers for different events
 let scatterPlotDispatcher = d3.dispatch(dispatchString);
 let calendarDispatcher = d3.dispatch(dispatchMonth);
 let filterDispatcher = d3.dispatch(dispatchLine);
@@ -12,8 +12,6 @@ let mapDispacher = d3.dispatch(dispatchString);
 //initialize scatterplot/dispatcher
 let scatterPlot = scatterplot().selectionDispatcher(scatterPlotDispatcher);
 scatterPlot();
-let mapLine = map().selectionDispatcher(scatterPlotDispatcher);
-mapLine();
 //initialize calendar/dispatcher
 let ourCalendar = calendar().selectionDispatcher(calendarDispatcher);
 ourCalendar();
@@ -21,10 +19,13 @@ ourCalendar();
 calendarDispatcher.on(dispatchMonth, function (monthString) {
     scatterPlot.updateSelection([monthString]);
 });
+
 let ourFilter = filter().selectionDispatcher(filterDispatcher);
-//listen for selected map event (this is currently the only linking we have)
+
+//listen for selected line event (this is currently the only linking we have)
 filterDispatcher.on(dispatchLine, function(lines){
     scatterPlot.updateSelection(["filter", lines])
+
 })
 
 ourFilter();
